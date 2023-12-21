cfc668b8ac9ec852f603758abaf8719bb303eb31
JaKooLit <jimmielovejay@gmail.com>
2023-12-21 23:49:47 +0900

    updated GTK theme install with tar.gz

--- install-scripts/gtk_themes.sh
+++ install-scripts/gtk_themes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+engine=(
+    gtk-engine-murrine
+)
+
 ############## WARNING DO NOT EDIT BEYOND THIS LINE if you dont know what you are doing! ######################################
 
 # Determine the directory where the script is located
@@ -20,7 +24,7 @@ YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
 # Set the name of the log file to include the current date and time
-LOG="install-$(date +%d-%H%M%S)_themes.log"
+LOG="install-$(date +%d-%H%M%S)_themes_main.log"
 
 
 ISAUR=$(command -v yay || command -v paru)
@@ -30,31 +34,31 @@ set -e
 
 # Function for installing packages
 install_package() {
-  # Checking if package is already installed
-  if $ISAUR -Q "$1" &>> /dev/null ; then
-    echo -e "${OK} $1 is already installed. Skipping..."
-  else
-    # Package not installed
-    echo -e "${NOTE} Installing $1 ..."
-    $ISAUR -S --noconfirm "$1" 2>&1 | tee -a "$LOG"
-    # Making sure package is installed
+    # Checking if package is already installed
     if $ISAUR -Q "$1" &>> /dev/null ; then
-      echo -e "\e[1A\e[K${OK} $1 was installed."
+        echo -e "${OK} $1 is already installed. Skipping..."
     else
-      # Something is missing, exiting to review log
-      echo -e "\e[1A\e[K${ERROR} $1 failed to install :( , please check the install.log. You may need to install manually! Sorry I have tried :("
-      exit 1
+        # Package not installed
+        echo -e "${NOTE} Installing $1 ..."
+        $ISAUR -S --noconfirm "$1" 2>&1 | tee -a "$LOG"
+        # Making sure package is installed
+        if $ISAUR -Q "$1" &>> /dev/null ; then
+            echo -e "\e[1A\e[K${OK} $1 was installed."
+        else
+            # Something is missing, exiting to review log
+            echo -e "\e[1A\e[K${ERROR} $1 failed to install :( , please check the install.log. You may need to install manually! Sorry I have tried :("
+            exit 1
+        fi
     fi
-  fi
 }
 
-# installing unzip package to unzip theme packages
-for PKG1 in unzip; do
-  install_package "$PKG1" 2>&1 | tee -a "$LOG"
-  if [ $? -ne 0 ]; then
-    echo -e "\033[1A\033[K${ERROR} - $PKG1 install had failed, please check the install.log"
-    exit 1
-  fi
+# installing engine needed for gtk themes
+for PKG1 in "${engine[@]}"; do
+    install_package "$PKG1" 2>&1 | tee -a "$LOG"
+    if [ $? -ne 0 ]; then
+        echo -e "\033[1A\033[K${ERROR} - $PKG1 install had failed, please check the install.log"
+        exit 1
+    fi
 done
 
 # Check if the directory exists and delete it if present
@@ -65,16 +69,16 @@ fi
 
 echo "$NOTE Cloning Tokyo Theme GTK themes and Icons repository..." 2>&1 | tee -a "$LOG"
 if git clone https://github.com/JaKooLit/GTK-themes-icons.git 2>&1 | tee -a "$LOG"; then
-    mkdir -p ~/.icons
-    mkdir -p ~/.themes
-    unzip -qo "GTK-themes-icons/theme/Tokyonight-Dark-BL-LB.zip" -d ~/.themes 2>&1 | tee -a "$LOG"
-    unzip -qo "GTK-themes-icons/theme/Tokyonight-Light-B-LB.zip" -d ~/.themes 2>&1 | tee -a "$LOG"
-    unzip -qo "GTK-themes-icons/icon/Tokyonight-Dark-Icons.zip" -d ~/.icons 2>&1 | tee -a "$LOG"
-    unzip -qo "GTK-themes-icons/icon/Tokyonight-Light-Icons.zip" -d ~/.icons 2>&1 | tee -a "$LOG"
+    cd GTK-themes-icons
+    chmod +x auto-extract.sh
+    ./auto-extract.sh 2>&1 | tee -a "$LOG"
+    cd ..
+    echo "$OK Extracted GTK Themes & Icons to ~/.icons & ~/.themes folders" 2>&1 | tee -a "$LOG"
 else
     echo "$ERROR Download failed for Tokyo Theme GTK themes and Icons.." 2>&1 | tee -a "$LOG"
 fi
 
 tar -xf "assets/Bibata-Modern-Ice.tar.xz" -C ~/.icons 2>&1 | tee -a "$LOG"
+echo "$OK Extracted Bibata-Modern-Ice.tar.xz to ~/.icons folder." 2>&1 | tee -a "$LOG"
 
 clear
