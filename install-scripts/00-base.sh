b540e8a9300db0eee82280066741811087dfe2a5
JaKooLit <jimmielovejay@gmail.com~>
2024-01-01 22:56:17 +0900

    tweaked abit

--- install-scripts/00-base.sh
+++ install-scripts/00-base.sh
@@ -6,14 +6,6 @@ base=(
   base-devel
 )
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-
-# Determine the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Change the working directory to the parent directory of the script
-PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || exit 1
-
 source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 
 # Set the name of the log file to include the current date and time
