e7d04b4f7bf3cb9bf98ea4434c961092ec344344
Ja.KooLit <ejhay.games@gmail.com>
2024-01-05 22:37:05 +0900

    resolving conflicts

--- install.sh
+++ install.sh
@@ -25,11 +25,25 @@ echo "$(tput setaf 3)NOTE: If you are installing on a VM, ensure to enable 3D ac
 echo
 
 read -p "$(tput setaf 6)Would you like to proceed? (y/n): $(tput sgr0)" proceed
+printf "\n%.0s" {1..2}
+echo "$(tput bold)$(tput setaf 7)Choose Y to use preset ONLY once you reviewed, updated or updated the preset.sh $(tput sgr0)" 
+echo "$(tput bold)$(tput setaf 7)If you are not sure what to do, choose N in the "Use Preset Settings" question $(tput sgr0)"
+printf "\n%.0s" {1..1}
+read -p "$(tput setaf 6)Would you like to Use Preset Settings? (y/n): $(tput sgr0)" use_preset
+
+printf "\n%.0s" {1..3}
 
 if [ "$proceed" != "y" ]; then
     echo "Installation aborted."
     exit 1
 fi
+
+
+
+
+
+
+
 
 # Create Directory for Install Logs
 if [ ! -d Install-Logs ]; then
