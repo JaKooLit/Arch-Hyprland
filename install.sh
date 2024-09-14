c0b198d562d1605936ed24273930f8a6a3bec78c
JaKooLit <ejhay.games@gmail.com>
2024-09-14 17:08:00 +0900

    Final tweaks of color variants for output messages

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 OK="$(tput setaf 2)[OK]$(tput sgr0)"
 ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
 NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
-WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
+WARN="$(tput setaf 5)[WARN]$(tput sgr0)"
 CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
 ORANGE=$(tput setaf 166)
 YELLOW=$(tput setaf 3)
