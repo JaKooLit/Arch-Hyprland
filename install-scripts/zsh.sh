cc9ce2cb4908684942f943fa274c93c3fa9c46f6
JaKooLit <ejhay.games@gmail.com>
2025-02-03 18:16:09 +0900

    minor zsh update

--- install-scripts/zsh.sh
+++ install-scripts/zsh.sh
@@ -73,23 +73,23 @@ fi
 
 # Install Oh My Zsh, plugins, and set zsh as default shell
 if command -v zsh >/dev/null; then
-  printf "${NOTE} Installing Oh My Zsh and plugins...\n"
+  printf "${NOTE} Installing ${SKY_BLUE}Oh My Zsh and plugins${RESET} ...\n"
   if [ ! -d "$HOME/.oh-my-zsh" ]; then
       sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || true
   else
-      echo "Directory .oh-my-zsh already exists. Skipping re-installation." 2>&1 | tee -a "$LOG"
+      echo "${INFO} Directory .oh-my-zsh already exists. Skipping re-installation." 2>&1 | tee -a "$LOG"
   fi
   # Check if the directories exist before cloning the repositories
   if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
       git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || true
   else
-      echo "Directory zsh-autosuggestions already exists. Skipping cloning." 2>&1 | tee -a "$LOG"
+      echo "${INFO} Directory zsh-autosuggestions already exists. Skipping cloning." 2>&1 | tee -a "$LOG"
   fi
 
   if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
       git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true
   else
-      echo "Directory zsh-syntax-highlighting already exists. Skipping cloning." 2>&1 | tee -a "$LOG"
+      echo "${INFO} Directory zsh-syntax-highlighting already exists. Skipping cloning." 2>&1 | tee -a "$LOG"
   fi
   
   # Check if ~/.zshrc and .zprofile exists, create a backup, and copy the new configuration
@@ -111,7 +111,7 @@ if command -v zsh >/dev/null; then
       echo "${ERROR} Authentication failed. Please enter the correct password." 2>&1 | tee -a "$LOG"
       sleep 1
   done
-  printf "${NOTE} Shell changed successfully to ${MAGENTA}zsh${RESET}" 2>&1 | tee -a "$LOG"
+  printf "${INFO} Shell changed successfully to ${MAGENTA}zsh${RESET}" 2>&1 | tee -a "$LOG"
 
 fi
 
