262b6eb87c461710cf6b4e6af4ff4042e8acaca2
JaKooLit <ejhay.games@gmail.com>
2025-02-05 11:44:49 +0900

    updated install.sh

--- install.sh
+++ install.sh
@@ -190,6 +190,7 @@ ask_yes_no "-Do you want to configure ${YELLOW}Bluetooth${RESET}?" bluetooth
 printf "\n"
 ask_yes_no "-Do you want to install ${YELLOW}Thunar file manager${RESET}?" thunar
 printf "\n"
+printf "${NOTE} ${YELLOW}AGS Desktop Overview DEMO link${RESET} on README\n"
 ask_yes_no "-Install ${YELLOW}AGS (aylur's GTK shell) v1${RESET} for Desktop-Like Overview?" ags
 printf "\n"
 ask_yes_no "-Install & configure ${YELLOW}SDDM${RESET} login manager, plus (OPTIONAL) SDDM theme?" sddm
