af4d191ee3aa2e067733086fa4d7e902d448473f
JaKooLit <ejhay.games@gmail.com>
2025-02-19 00:09:49 +0900

    added mercurial for the new oh my zsh theme

--- install-scripts/zsh_pokemon.sh
+++ install-scripts/zsh_pokemon.sh
@@ -2,6 +2,10 @@
 # 💫 https://github.com/JaKooLit 💫 #
 # pokemon-color-scripts#
 
+poke=(
+  mercurial
+)
+
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
 # Determine the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -15,8 +19,21 @@ source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 # Set the name of the log file to include the current date and time
 LOG="Install-Logs/install-$(date +%d-%H%M%S)_zsh_pokemon.log"
 
+# for the new oh my zsh theme
+printf "${NOTE} Installing ${SKY_BLUE}additional dependencies for oh my zsh theme${RESET}\n" 
+for pok in "${poke[@]}"; do
+  install_package "$pok" "$LOG"
+done
+printf "\n%.0s" {1..1}
+
 printf "${INFO} Installing ${SKY_BLUE}Pokemon color scripts${RESET} ..."
-install_package 'pokemon-colorscripts-git' "$LOG"
+
+if [ -d "pokemon-colorscripts" ]; then
+    cd pokemon-colorscripts && git pull && sudo ./install.sh && cd ..
+    else
+    git clone --depth 1 https://gitlab.com/phoneybadger/pokemon-colorscripts.git &&
+    cd pokemon-colorscripts && sudo ./install.sh && cd ..
+fi
 
 # Check if ~/.zshrc exists
 if [ -f "$HOME/.zshrc" ]; then
