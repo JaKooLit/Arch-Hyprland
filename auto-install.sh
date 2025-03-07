11a27c79b9fc8de50bd7e68e8db59c9e0b7680e9
JaKooLit <ejhay.games@gmail.com>
2025-03-07 18:48:53 +0900

    added some color coding

--- auto-install.sh
+++ auto-install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # https://github.com/JaKooLit
 
+# Set some colors for output messages
+OK="$(tput setaf 2)[OK]$(tput sgr0)"
+ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
+WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+MAGENTA="$(tput setaf 5)"
+ORANGE="$(tput setaf 214)"
+WARNING="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+GREEN="$(tput setaf 2)"
+BLUE="$(tput setaf 4)"
+SKY_BLUE="$(tput setaf 6)"
+RESET="$(tput sgr0)"
+
+# Variables
 Distro="Arch-Hyprland"
 Github_URL="https://github.com/JaKooLit/$Distro.git"
 Distro_DIR="$HOME/$Distro"
@@ -9,9 +26,9 @@ printf "\n%.0s" {1..1}
 
 if ! command -v git &> /dev/null
 then
-    echo "Git not found! Installing Git..."
+    echo "${INFO} Git not found! ${SKY_BLUE}Installing Git...${RESET}"
     if ! sudo pacman -S git --noconfirm; then
-        echo "Failed to install Git. Exiting."
+        echo "${ERROR} Failed to install Git. Exiting."
         exit 1
     fi
 fi
@@ -19,13 +36,13 @@ fi
 printf "\n%.0s" {1..1}
 
 if [ -d "$Distro_DIR" ]; then
-    echo "$Distro_DIR exists. Updating the repository..."
+    echo "${YELLOW}$Distro_DIR exists. Updating the repository... ${RESET}"
     cd "$Distro_DIR"
     git stash && git pull
     chmod +x install.sh
     ./install.sh
 else
-    echo "$Distro_DIR does not exist. Cloning the repository..."
+    echo "${MAGENTA}$Distro_DIR does not exist. Cloning the repository...${RESET}"
     git clone --depth=1 "$Github_URL" "$Distro_DIR"
     cd "$Distro_DIR"
     chmod +x install.sh
