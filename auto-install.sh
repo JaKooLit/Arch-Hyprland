279536e69b8c992c9cec1f2bbb4b20573825cbc9
JaKooLit <ejhay.games@gmail.com>
2025-03-07 15:14:44 +0900

    update

--- auto-install.sh
+++ auto-install.sh
@@ -9,10 +9,23 @@ then
     sudo pacman -S --no-confirm git
 fi
 
-# Clone the repository
-echo "Cloning the repository..."
-git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git -b development ~/Arch-Hyprland
-cd ~/Arch-Hyprland
-chmod +x install.sh
-./install.sh
+# Define the directory path
+Distro_DIR="$HOME/Arch-Hyprland"
+
+# Check if the repository already exists
+if [ -d "$Distro_DIR" ]; then
+    cd "$Distro_DIR"
+    git stash && git pull
+    chmod +x install.sh
+    ./install.sh
+else
+    # Clone the repository if it doesn't exist
+    echo "$Distro_DIR does not exist. Cloning the repository..."
+    git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git -b development "$Distro_DIR"
+    cd "$Distro_DIR"
+    chmod +x install.sh
+    echo "Running install.sh..."
+    ./install.sh
+fi
+
 
