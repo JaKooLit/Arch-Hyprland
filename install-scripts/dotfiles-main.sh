ec1285d8d142c5db59f65353797fadc28a7f0a21
JaKooLit <ejhay.games@gmail.com>
2025-03-09 16:13:20 +0900

    updated dotfiles.sh

--- install-scripts/dotfiles-main.sh
+++ install-scripts/dotfiles-main.sh
@@ -21,9 +21,7 @@ printf "${NOTE} Cloning and Installing ${SKY_BLUE}KooL's Hyprland Dots${RESET}..
 
 if [ -d Hyprland-Dots ]; then
   cd Hyprland-Dots
-  git stash
-  git pull
-  git stash apply
+  git stash && git pull
   chmod +x copy.sh
   ./copy.sh 
 else
